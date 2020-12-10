0e89ee1894f7eaab734a574bb1fe686ceca2b65f
keynes <keyneskanno@gmail.com>
2020-12-10 14:10:01 -0300

    login inserido

--- lib/main.dart
+++ lib/main.dart
@@ -60,6 +60,7 @@ class Home extends StatelessWidget {
                   Text(
                     "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
                     style: TextStyle(
+
                         color: Colors.grey,
                         fontSize: 18.0,
                         fontWeight: FontWeight.bold
